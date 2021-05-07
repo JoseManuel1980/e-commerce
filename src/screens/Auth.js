a97acdabd27ccd2c923d04543fb08bf0836861d7
Jose Palacio <jose_palacio@gmail.com>
2021-05-07 23:01:17 +0200

    .
    Signed-off-by: Jose Palacio <jose_palacio@gmail.com>

---
+++ src/screens/Auth.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+} from "react-native";
+import logo from "./../../assets/logoPez.png";
+import logo2 from "./../../assets/logo.png";
+import { layoutStyle } from "./../styles/index";
+import RegisterForm from "./../components/Auth/RegisterForm";
+import LoginForm from "./../components/Auth/LoginForm";
+
+export default function Auth() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const changeForm = () => setShowLogin(!showLogin);
+
+  return (
+    <View style={[layoutStyle.container]}>
+      <Image style={styles.logo} source={logo} />
+      <Image style={styles.logo2} source={logo2} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        {showLogin ? (
+          <LoginForm changeForm={changeForm} />
+        ) : (
+          <RegisterForm changeForm={changeForm} />
+        )}
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 200,
+    resizeMode: "contain",
+    marginBottom: 20,
+  },
+  logo2: {
+    width: "100%",
+    height: 50,
+    resizeMode: "contain",
+    marginBottom: 20,
+    tintColor: "#0098d3",
+  },
+});
